0b8b3164917c3eeb960141600e83c298a665d1a0
Orlando Yurivilca <sergioyv@gmail.com>
2022-01-17 15:00:14 -0500

    2do chall

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # aca pongo las rutas (qe van dirigidas al controller)
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
